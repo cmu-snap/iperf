41d4ba84acd3b861aa318ba5944124864242bd1f
Christopher Canel <c.canel@icloud.com>
2024-06-10 19:47:53 +0000

    Fix

--- src/iperf_client_api.c
+++ src/iperf_client_api.c
@@ -669,13 +669,13 @@ int iperf_run_client(struct iperf_test *test) {
              test->blocks_received >= test->settings->blocks)))) {
         // Check if we need to start the next burst.
         ++test->bursts_sent;
-        printf(test, "Finished %lu bursts\n", test->bursts_sent);
+        printf("Finished %lu bursts\n", test->bursts_sent);
         if (test->bursts_sent < test->settings->num_bursts) {
-          printf(test, "Inter-burst wait time: %d us\n",
+          printf("Inter-burst wait time: %d us\n",
                  test->settings->inter_burst_time_us);
           usleep((unsigned int)test->settings->inter_burst_time_us);
 
-          printf(test, "Starting burst %lu\n", test->bursts_sent + 1);
+          printf("Starting burst %lu\n", test->bursts_sent + 1);
           test->bytes_sent = 0;
           SLIST_FOREACH(sp, &test->streams, streams) {
             if (sp->sender) {
